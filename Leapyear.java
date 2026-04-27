2dfda98638cc2e3d2318394c3dd59170f7e3043d
arun kumar <arun.kumar@cogniwide.com>
2026-04-27 11:28:40 +0530

    first commit

---
+++ Leapyear.java
@@ -0,0 +1,12 @@
+// To find the Leap year or not
+
+public class LeapYearCheck {
+   public static void main(String[] args) {
+       int year = 2024;
+       if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+           System.out.println(year + " is a leap year.");
+       } else {
+           System.out.println(year + " is not a leap year.");
+       }
+   }
+}
