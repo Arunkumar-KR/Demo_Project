2f414863e9efe9c67a2942c2e4e36f4608efdeff
arun kumar <arun.kumar@cogniwide.com>
2026-04-27 15:41:25 +0530

    seventh commit

--- Leapyear.java
+++ Leapyear.java
@@ -4,7 +4,7 @@
 // modified good
 
 
-public class LeapYearCheck {
+public class Leapyear {
    public static void main(String[] args) {
        int year = 2024;
        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
